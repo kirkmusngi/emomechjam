9564fd26e674bed9c50d8fa3a7053906e3eb8c41
Kirk Musngi <kirk.musngi@gmail.com>
2019-02-08 14:31:30 -0500

    Add all project files to repository
    Initial commit after GitHub package setup

---
+++ Assets/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* TO-DO 
+ * - how to flesh out the dialogue bits
+ * - how can we feed written dialogue easily
+ * - 
+ * 
+ * 
+ */
+
+public class Actor : MonoBehaviour
+{
+    public new string name;
+    public List<string> dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Dialogue currentDialogue;
+
+    public Image actorAvatar;
+    public Text actorName;
+    public Text actorSpeech;
+
+    Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        Debug.Log("Starting conversation with " + currentDialogue.currentActor);
+
+        sentences.Clear();
+
+        foreach(string sentence in currentDialogue.lines)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string nextSentence = sentences.Dequeue();
+        StartCoroutine(DisplaySentence(nextSentence));
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of dialogue!");
+    }
+
+    IEnumerator DisplaySentence(string sentence)
+    {
+        actorName.text = currentDialogue.currentActor;
+        actorSpeech.text = "";
+
+        foreach(char letter in sentence.ToCharArray())
+        {
+            actorSpeech.text += letter;
+            yield return null;
+        }
+    }
+}
